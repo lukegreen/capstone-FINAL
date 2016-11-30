0274ba272d885099267801251fde71c1406efd55
Luke Green <elukuent@gmail.com>
2016-11-29 19:33:22 -0800

    scaffolded final project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capstoneFinalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the capstoneFinalApp
+ */
+angular.module('capstoneFinalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capstoneFinalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the capstoneFinalApp
+ */
+angular.module('capstoneFinalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
